3cf1319e8e0be672805c10a08af4c8a8203be3aa
Johnnatan <joh.villaneda@outlook.com>
2021-12-09 09:33:34 -0500

    Finished project

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ let btn = document.querySelector("#action");
 
 let action = () => {
   btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
     document.querySelector("#tooltip").classList.toggle("action");
   });
 };
